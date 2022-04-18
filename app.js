9644c6bb4e374b0c7b34b617d64a3b6bde83a656
ismaeelhaider12 <90700806+ismaeelhaider12@users.noreply.github.com>
2022-04-19 02:08:10 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Express Demo App</h1> <h4>Hello this is warning messsage from ismaeel haider the original master</h4> <p>Versiosn 1.2</p>');
+})
+
+app.get('/products', (req, res) => {
+  res.send([
+    {
+      productId: '103',
+      price: 100
+    },
+    {
+      productId: '102',
+      price: 150
+    }
+  ])
+})
+
+app.listen(port, ()=> {
+  console.log(`Demo app is up and listening to port: ${port}`);
+})
+ 
